b43a6ba2ddac7eecd6a4f450ac5329eba725a704
valentin.wynands <valentin.wynands@epsi.fr>
2021-01-11 11:08:19 +0100

    first commit

---
+++ data/catalogue.js
@@ -0,0 +1,77 @@
+var catalog = [
+
+    {
+        "name" : "Ecran LG #1",
+        "description" : "Ecran d'une très grande qualité",
+        "image" : "data/images/EcranLG.jpg",
+        "prix" : "420"},
+
+    {
+        "name" : "Ecran LG #2",
+        "description" : "Ecran d'une très grande qualité",
+        "image" : "data/images/EcranLG2.jpg",
+        "prix" : "261"},
+
+    {
+        "name" : "Ecran LG #3",
+        "description" : "Ecran d'une très grande qualité",
+        "image" : "data/images/EcranLG3.jpg",
+        "prix" : "156"},
+
+    {
+        "name" : "Ecran LG #4",
+        "description" : "Ecran d'une très grande qualité",
+        "image" : "data/images/EcranLG4.jpg",
+        "prix" : "127"},
+
+    {
+        "name" : "Ecran Acer #1",
+        "description" : "Ecran d'une très grande qualité",
+        "image" : "data/images/EcranACER.jpg",
+        "prix" : "221"},
+
+    {
+        "name" : "Ecran Acer #2",
+        "description" : "Ecran d'une très grande qualité",
+        "image" : "data/images/EcranACER2.jpg",
+        "prix" : "596"},
+
+    {
+        "name" : "Ecran Acer #3",
+        "description" : "Ecran d'une très grande qualité",
+        "image" : "data/images/EcranACER3.jpg",
+        "prix" : "96"},
+
+    {
+        "name" : "Ecran Acer #4",
+        "description" : "Ecran d'une très grande qualité",
+        "image" : "data/images/EcranACER4.jpg",
+        "prix" : "234"},
+
+    {
+        "name" : "Ecran benQ #1",
+        "description" : "Ecran d'une très grande qualité",
+        "image" : "data/images/EcranbenQ.jpg",
+        "prix" : "195"},
+
+    {
+        "name" : "Ecran benQ #2",
+        "description" : "Ecran d'une très grande qualité",
+        "image" : "data/images/EcranbenQ2.jpg",
+        "prix" : "157"},
+
+    {
+        "name" : "Ecran benQ #2",
+        "description" : "Ecran d'une très grande qualité",
+        "image" : "data/images/EcranbenQ3.jpg",
+        "prix" : "256"},
+
+    {
+        "name" : "Ecran benQ #4",
+        "description" : "Ecran d'une très grande qualité",
+        "image" : "data/images/EcranbenQ4.jpg",
+        "prix" : "340"},
+
+];
+
+
