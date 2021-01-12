3d099a974e71fa533150cefd6678c46b07ca5cc9
valentin.wynands <valentin.wynands@epsi.fr>
2021-01-12 14:08:01 +0100

    projet final

--- scripts/main.js
+++ scripts/main.js
@@ -1,138 +1,130 @@
-// YOUR NAME HERE
 
-// === constants ===
+// Constantes
 const MAX_QTY = 9;
 const productIdKey = "product";
 const orderIdKey = "order";
-const inputIdKey = "qte";
+const inputIdKey = "quantite";
+const achats = [];
 
-// === global variables  ===
-// the total cost of selected products 
 var total = 0;
 
-// function called when page is loaded, it performs initializations 
-var init = function () {
-	createShop();
-	
-	// TODO : add other initializations to achieve if you think it is required
+function init() {
+    createShop();
 }
+
 window.addEventListener("load", init);
 
-
-
-// usefull functions
-
-/*
-* create and add all the div.produit elements to the div#boutique element
-* according to the product objects that exist in 'catalog' variable
-*/
-var createShop = function () {
-	var shop = document.getElementById("boutique");
-	for(var i = 0; i < catalog.length; i++) {
-		shop.appendChild(createProduct(catalog[i], i));
-	}
+function createShop() {
+    var shop = document.getElementById("boutique");
+    for (var i = 0; i < catalog.length; i++) {
+        shop.appendChild(createProduct(catalog[i], i));
+    }
 }
 
-/*
-* create the div.produit elment corresponding to the given product
-* The created element receives the id "index-product" where index is replaced by param's value
-* @param product (product object) = the product for which the element is created
-* @param index (int) = the index of the product in catalog, used to set the id of the created element
-*/
-var createProduct = function (product, index) {
-	// build the div element for product
-	var block = document.createElement("div");
-	block.className = "produit";
-	// set the id for this product
-	block.id = index + "-" + productIdKey;
-	// build the h4 part of 'block'
-	block.appendChild(createBlock("h4", product.name));
-	// /!\ should add the figure of the product... does not work yet... /!\ 
-	// block.appendChild(createFigureBlock(product));
-	block.appendChild(createBlock("img", product.image, "imgproduct"));
-	// build and add the div.description part of 'block' 
-	block.appendChild(createBlock("div", product.description, "description"));
-	// build and add the div.price part of 'block'
-	block.appendChild(createBlock("div", product.prix, "prix"));
-	// build and add control div block to product element
-	block.appendChild(createOrderControlBlock(index));
-	return block;
+function createProduct(product, index) {
+    var block = document.createElement("div");
+    block.className = "produit";
+    block.id = index + "-" + productIdKey;
+
+    block.appendChild(createBlock("h4", product.name));
+    block.appendChild(createBlock("img", product.image, "imgproduct"));
+    block.appendChild(createBlock("div", product.description, "description"));
+    block.appendChild(createBlock("div", product.prix, "prix"));
+    block.appendChild(createOrderControlBlock(index));
+    return block;
 }
 
-
-/* return a new element of tag 'tag' with content 'content' and class 'cssClass'
- * @param tag (string) = the type of the created element (example : "p")
- * @param content (string) = the html wontent of the created element (example : "bla bla")
- * @param cssClass (string) (optional) = the value of the 'class' attribute for the created element
- */
-var createBlock = function (tag, content, cssClass) {
-	var element = document.createElement(tag);
-	if (cssClass != undefined) {
-		element.className =  cssClass;
-	}
-	if(tag === 'img')
-		element.src = content
-	else
-		element.innerHTML = content;
-	return element;
+function createBlock(tag, content, cssClass) {
+    var element = document.createElement(tag);
+    if (cssClass != undefined) {
+        element.className = cssClass;
+    }
+    if (tag === 'img')
+        element.src = content
+    else
+        element.innerHTML = content;
+    return element;
 }
 
-/*
-* builds the control element (div.controle) for a product
-* @param index = the index of the considered product
-*
-* TODO : add the event handling, 
-*   /!\  in this version button and input do nothing  /!\  
-*/
-var createOrderControlBlock = function (index) {
-	var control = document.createElement("div");
-	control.className = "controle";
+function createOrderControlBlock(index, product) {
+    var control = document.createElement("div");
+    control.className = "controle";
+    var input = document.createElement("input");
+    input.id = index + '-' + inputIdKey;
+    input.type = "number";
+    input.step = "1";
+    input.value = "0";
+    input.min = "0";
+    input.max = MAX_QTY.toString();
+    control.appendChild(input);
 
-	// create input quantity element
-	var input = document.createElement("input");
-	input.id = index + '-' + inputIdKey;
-	input.type = "number";
-	input.step = "1";
-	input.value = "0";
-	input.min = "0";
-	input.max = MAX_QTY.toString();
-	// add input to control as its child
-	control.appendChild(input);
-
-	// create order button
-	var button = document.createElement("button");
-	button.className = 'commander';
-	button.id = index + "-" + orderIdKey;
-	// add control to control as its child
-	control.appendChild(button);
-
-	// the built control div node is returned
-	return control;
+    var button = document.createElement("button");
+    button.className = 'commander';
+    button.innerHTML = "Ajouter"
+    button.id = index + "-" + orderIdKey;
+    control.appendChild(button);
+    button.addEventListener ("click", function() {
+        alert("Article ajouté au panier");
+    });
+    return control;
 }
 
-
-/*
-* create and return the figure block for this product
-* see the static version of the project to know what the <figure> should be
-* @param product (product object) = the product for which the figure block is created
-*
-* TODO : write the correct code
-*/
-var createFigureBlock = function (product) {
-	// this is absolutely not the correct answer !
-	// TODO 
-	return createBlock("figure", "");
+function updateButton(input) {
+    if (input.value === 0) {
+        input.value = null;
+        let id = input.id.substring(9);
+        let button = document.getElementById("produit-" + id);
+        disableInput(button);
+    } else {
+        let id = input.id.substring(9);
+        let button = document.getElementById("produit-" + id);
+        enableInput(input.value, id, button);
+    }
 }
 
+function enableButton(quantite, id, button) {
+    button.removeAttribute('disabled');
+    button.onclick = function () {
+        addToCart(id, parseInt(quantite));
+        const input = document.getElementById("quantite-" + id);
+        input.value = null;
+    };
+    button.setAttribute('style', 'opacity:1');
+}
+
+function disableButton(button) {
+    button.removeAttribute("onclick");
+    button.setAttribute("disabled", "");
+    button.setAttribute('style', 'opacity:0.15');
+}
+
+function addToCart(id, quantite){
+
+}
+
+function deleteProductFromCart(id) {
+    const index = achats.indexOf(element => element.product.id === id)
+    achats.splice(index, 1);
+    document.getElementById("achat-" + id).remove();
+    getTotalPrice();
+}
+
+function montant() {
+    let sum = 0;
+    achats.forEach(achat => {
+        sum += achat.quantite * parseInt(achat.product["prix"]);
+    });
+    document.getElementById('total-price').innerHTML = sum;
+}
 
 // BARRE DE RECHERCHE
 
-function rechercherArticle(libelle) {
-	var articlesCorrespondants = [];
-	catalog.forEach(produit => {
-		if (produit["name"].toUpperCase().includes(libelle.toUpperCase())) {
-			articlesCorrespondants.push(produit);
-		}
-	});
-	generateItemList(articlesCorrespondants);
+function searchProduct(search) {
+    var listProducts = [];
+    catalog.forEach(product => {
+        if (product["name"].toUpperCase().includes(search.toUpperCase())) {
+            listProducts.push(product);
+        }
+    });
+    createShop(listProducts);
 }
